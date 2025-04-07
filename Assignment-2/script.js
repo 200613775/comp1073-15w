930e7e9127beacf5e2cd4ea152b167a16f6e2df6
Nishi Patel <patelnishi369@gmail.com>
2025-04-07 16:32:11 -0400

    Completed Assignment's required part

--- Assignment-2/script.js
+++ Assignment-2/script.js
@@ -1,7 +1,8 @@
 const output = document.getElementById('output');
-// create a class for the smoothie order
+
+// Create a class for the smoothie order
 class SmoothieOrder {
-    // create a constructors for the the class
+    // Constructor for the class
     constructor(firstName, lastName, size, flavours, toppings) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -10,15 +11,53 @@ class SmoothieOrder {
         this.toppings = toppings;
     }
 
-    // get the description of the smoothie order
-    getDescription(){
+    // Get the description of the smoothie order
+    getDescription() {
         let flavoursList = this.flavours;
         let toppingsList = this.toppings;
-        returm `${this.firstName} ${this.lastName} give a order ${this.size}size smoothie with${flavoursList} flavour and ${toppingsList}topping.`;
+        return `${this.firstName} ${this.lastName} gave an order for a ${this.size} size smoothie with ${flavoursList} flavours and ${toppingsList} toppings.`;
     }
-};
-
-// add eventListener for the form submission
-FormData.addEventListenet('submit', function(event)) {
-    event.preventDefault();
 }
+
+// Add event listener for the form submission
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Get the details of the customer like name
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+
+    // Get the information about the size
+    const sizeElements = document.getElementsByName('size');
+    let size = '';
+    for (const element of sizeElements) {
+        if (element.checked) {
+            size = element.value;
+        }
+    }
+
+    // Get selected flavours
+    const flavourElements = document.getElementsByName('flavour');
+    const selectedFlavours = [];
+    for (const element of flavourElements) {
+        if (element.checked) {
+            selectedFlavours.push(element.value);
+        }
+    }
+
+    // Get selected toppings
+    const toppingElements = document.getElementsByName('topings');
+    const selectedToppings = [];
+    for (const element of toppingElements) {
+        if (element.checked) {
+            selectedToppings.push(element.value);
+        }
+    }
+
+    // Create a new object for the smoothie order
+    const smoothie = new SmoothieOrder(firstname, lastname, size, selectedFlavours, selectedToppings);
+
+    // Show the description of the order
+    const smoothieDescription = smoothie.getDescription();
+    output.textContent = `${smoothieDescription}`;
+});
