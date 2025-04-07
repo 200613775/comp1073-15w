0ae460783bd3abd575538113ecbf1e20a326dfbd
Nishi Patel <patelnishi369@gmail.com>
2025-04-06 20:29:35 -0400

    Created a class and constructor for the assignment

---
+++ Assignment-2/script.js
@@ -0,0 +1,12 @@
+const output = document.getElementById('output');
+// create a class for the smoothie order
+class SmoothieOrder {
+    // create a constructors for the the class
+    constructor(firstName, lastName, size, flavours, toppings) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.size = size;
+        this.flavours = flavours;
+        this.toppings = toppings;
+    }
+}
